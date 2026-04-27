7f6d3fee260fbb7aaa1369f6922faa12a3eeedc7
jacky <Jacky.ni@wohuimandarin.com>
2026-04-27 13:33:32 +0800

    feat: 添加权限标识

--- api/admin/user/v1/user_kick.go
+++ api/admin/user/v1/user_kick.go
@@ -3,7 +3,7 @@ package v1
 import "github.com/gogf/gf/v2/frame/g"
 
 type UserKickSessionsReq struct {
-	g.Meta `path:"/user/{id}/kick-sessions" method:"post" tags:"用户管理" summary:"强制下线该用户全部会话"`
+	g.Meta `path:"/user/{id}/kick-sessions" method:"post" tags:"用户管理" summary:"强制下线该用户全部会话" permission:"user:kick_sessions"`
 	Id     int64 `json:"id" in:"path" v:"required#err.invalid_params" dc:"用户ID"`
 }
 
