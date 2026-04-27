02925a72738d3f116218da2056d28c38b45de8b2
jacky <Jacky.ni@wohuimandarin.com>
2026-04-27 16:26:06 +0800

    feat: 删除cors

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -4,6 +4,6 @@ import "github.com/gogf/gf/v2/net/ghttp"
 
 // MiddlewareCORS is a middleware handler for CORS with default options.
 func MiddlewareCORS(r *ghttp.Request) {
-	r.Response.CORSDefault()
+	//r.Response.CORSDefault()
 	r.Middleware.Next()
 }
