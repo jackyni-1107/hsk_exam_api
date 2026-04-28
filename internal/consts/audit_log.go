3f873907670b03b8eaf392a5c1344ec42bdc167e
jacky <Jacky.ni@wohuimandarin.com>
2026-04-28 09:53:55 +0800

    fixed: 统一登录日志类型

--- internal/consts/audit_log.go
+++ internal/consts/audit_log.go
@@ -2,9 +2,9 @@ package consts
 
 // 登录审计日志类型（sys_login_log.log_type 等）
 const (
-	AuditLogTypeLoginSuccess = 1
-	AuditLogTypeLoginFail    = 2
-	AuditLogTypeLogout       = 3
+	AuditLogTypeLoginSuccess = "login_success"
+	AuditLogTypeLoginFail    = "login_fail"
+	AuditLogTypeLogout       = "logout"
 )
 
 const (
